df15d52dae36d616e8594fb2d9c29701a7676def
Avinash A S <121566791+avinashsatalagaon@users.noreply.github.com>
2026-01-12 23:57:55 +0530

    Time: 344 ms (68.07%) | Memory: 0B (100.00%) - LeetSync

--- 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -1,26 +1,17 @@
 # Write your MySQL query statement below
 WITH FilteredStadium AS (
-    -- Step 1: Filter and calculate the group identifier
-    SELECT 
-        id, 
-        visit_date, 
-        people,
+    SELECT id, visit_date,  people,
         id - ROW_NUMBER() OVER (ORDER BY id) AS grp
     FROM Stadium
     WHERE people >= 100
 ),
 GroupCounts AS (
-    -- Step 2: Count how many records are in each group
     SELECT 
-        *,
-        COUNT(*) OVER (PARTITION BY grp) AS count_in_grp
+        *, COUNT(*) OVER (PARTITION BY grp) AS count_in_grp
     FROM FilteredStadium
 )
--- Step 3: Return only rows belonging to groups of 3+
-SELECT 
-    id, 
-    visit_date, 
-    people
+-- Return only rows belonging to groups of 3+
+SELECT id, visit_date, people
 FROM GroupCounts
 WHERE count_in_grp >= 3
 ORDER BY visit_date;
